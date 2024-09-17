69ef7166be4ea112fef857cbb5160f628ff3746f
Wesley Costa <wesleyeugenio3@gmail.com>
2024-09-17 18:23:26 -0300

    fix: api link

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://dindin-app-backend.vercel.app/",
   timeout: 3000,
   headers: { "Content-Type": "application/json" },
 });
